7c9c619e07fe304319c87a48d85d51e384eaa593
tompro <office@protom.eu>
2025-10-01 15:11:04 +0200

    Increase time slack to 24 hours

--- crates/bcr-ebill-api/src/constants.rs
+++ crates/bcr-ebill-api/src/constants.rs
@@ -6,5 +6,5 @@ pub const MAX_FILE_NAME_CHARACTERS: usize = 200;
 pub const VALID_FILE_MIME_TYPES: [&str; 3] = ["image/jpeg", "image/png", "application/pdf"];
 
 // When subscribing events we subtract this from the last received event time
-pub const NOSTR_EVENT_TIME_SLACK: u64 = 3600; // 1 hour
+pub const NOSTR_EVENT_TIME_SLACK: u64 = 3600 * 24; // 1 day
 pub const CURRENCY_SAT: &str = "sat";
