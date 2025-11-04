1cc67079ffb6aef43c77660a8fcd6ec032eba88f
tompro <office@protom.eu>
2025-11-04 14:01:20 +0100

    Extract propagation service

--- crates/bcr-ebill-api/src/service/notification_service/service.rs
+++ crates/bcr-ebill-api/src/service/notification_service/service.rs
@@ -20,11 +20,33 @@ use std::collections::HashMap;
 #[cfg(test)]
 use mockall::automock;
 
+/// Methods required for all block propagations and chain re-syncs
+#[allow(dead_code)]
+#[cfg_attr(test, automock)]
+#[cfg_attr(target_arch = "wasm32", async_trait(?Send))]
+#[cfg_attr(not(target_arch = "wasm32"), async_trait)]
+pub trait BlockPropagationServiceApi {
+    /// Sent when an identity chain is created or updated
+    async fn send_identity_chain_events(&self, events: IdentityChainEvent) -> Result<()>;
+    /// Sent when a company chain is created or updated
+    async fn send_company_chain_events(&self, events: CompanyChainEvent) -> Result<()>;
+    /// Sent when: A bill chain is created or updated
+    async fn send_bill_chain_events(&self, events: BillChainEvent) -> Result<()>;
+    /// Resync bill chain
+    async fn resync_bill_chain(&self, bill_id: &BillId) -> Result<()>;
+    /// Resync company chain
+    async fn resync_company_chain(&self, company_id: &NodeId) -> Result<()>;
+    /// Resync identity chain
+    async fn resync_identity_chain(&self) -> Result<()>;
+}
+
+#[cfg(test)]
+impl ServiceTraitBounds for MockBlockPropagationServiceApi {}
+
 #[cfg(test)]
 impl ServiceTraitBounds for MockNotificationServiceApi {}
 
 /// Send events via all channels required for the event type.
-#[allow(dead_code)]
 #[cfg_attr(test, automock)]
 #[cfg_attr(target_arch = "wasm32", async_trait(?Send))]
 #[cfg_attr(not(target_arch = "wasm32"), async_trait)]
